82790da2ec1d59d7f408e188312f3cb70ff4bf08
Kevin Ha <kevinha298@live.com>
2021-09-17 22:09:32 -0700

    Registered admin model.

--- djangoapp/api/models.py
+++ djangoapp/api/models.py
@@ -19,3 +19,6 @@ class Users(models.Model):
         return path
     
     resume = models.ImageField(upload_to=upload_file, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.employeeID} - {self.employeeName}'

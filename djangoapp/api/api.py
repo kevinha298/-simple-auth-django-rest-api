9599e6ed2412b704e05c83b2a72876707dcb0ba2
Kevin Ha <kevinha298@live.com>
2021-09-18 21:08:39 -0700

    Added post method.

--- djangoapp/api/api.py
+++ djangoapp/api/api.py
@@ -9,4 +9,9 @@ class UserList(APIView):
         serializer = UserSerializer(model, many=True)
         return Response(serializer.data)
 
-
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

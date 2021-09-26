3aa20a57ee31d48b2453484ed7d8e32bcca95624
Kevin Ha <kevinha298@live.com>
2021-09-25 21:35:44 -0700

    Added authentication token.

--- djangoapp/MainApp/urls.py
+++ djangoapp/MainApp/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url 
 
-from api.api import UserList, UserDetail
+from api.api import UserList, UserDetail, UserAuthentication
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/users_list/$', UserList.as_view(), name='user_list'),
-    url(r'^api/users_list/(?P<employeeID>\d+)/$', UserDetail.as_view(), name='user_list')
+    url(r'^api/users_list/(?P<employeeID>\d+)/$', UserDetail.as_view(), name='user_list'),
+    url(r'^api/auth/$', UserAuthentication.as_view(), name='User Authentication API'),
 ]
